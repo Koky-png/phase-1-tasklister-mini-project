5340b1e28a319b6557d771912e6c81ebc43d59d6
Njoki <kokymash@gmail.com>
2024-10-06 23:40:26 +0300

    Add myfile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault();
+  const form = document.getElementById("create-task-form");
+  const list = document.getElementById("tasks");
+ form.onsubmit = (e) =>{
+   e.preventDefault();
+   task = document.getElementById("new-task-description").value;
+   color = document.getElementById("select_colors").value
+   list.appendChild(addTodoList(task,color))
+   form.reset()
+ }
+ function addTodoList(todo,colors){
+   let lists = document.createElement("li")
+   lists.textContent = todo;
+   lists.style.color = colors;
+   // add delete button
+   let deleteButton = document.createComment("button");
+   deleteButton.textContent = "x";
+   lists.appendChild(deleteButton);
+   deleteButton.onclick = () =>{
+     lists.remove()
+   }
+   return lists;
+ }
+ });
